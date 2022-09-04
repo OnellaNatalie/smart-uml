89e78cc0b2ce0f2996b6a3bd4d10118d71e7c112
jayasith <it19079264@my.sliit.lk>
2022-09-04 15:59:36 +0530

    feat(useCaseDiagramModelDetection) create end point for save text associated with the use cases and actors

--- backend/services/submission_service.py
+++ backend/services/submission_service.py
@@ -1,6 +1,5 @@
 import os
 import app
-from datetime import datetime
 
 from config.database import db
 from models.class_diagram_answer import ClassAnswer
@@ -8,8 +7,6 @@ from models.use_case_answer import UseCaseAnswer
 
 
 def save_submission(assignment_id, image, submission_type, comment, user_id):
-    # date_now = str(datetime.now())
-    # file_name = image.filename + '-' + date_now+'.jpg'
 
     if submission_type == 'use case':
         image.save(os.path.join(app.SUBMISSION_PATH, image.filename))
@@ -17,9 +14,11 @@ def save_submission(assignment_id, image, submission_type, comment, user_id):
                                      comments=comment)
         db.session.add(use_case_obj)
         db.session.commit()
+        return use_case_obj
     else:
         image.save(os.path.join(app.SUBMISSION_PATH, image.filename))
         class_obj = ClassAnswer(user=user_id, assignment=assignment_id, file_name=image.filename,
                                 comments=comment)
         db.session.add(class_obj)
         db.session.commit()
+        return class_obj
