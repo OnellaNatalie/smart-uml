3da1a7fe04465a9ba32f218a8d1c6866851d97b1
Weerasinghe D.N.H <it19070704@my.sliit.lk>
2022-09-09 14:10:16 +0530

    class_model_detection_service.py methods change

--- backend/services/class_model_detection_service.py
+++ backend/services/class_model_detection_service.py
@@ -3,7 +3,7 @@ import re
 
 import cv2
 import numpy as np
-import pytesseract as pytesseract
+import pytesseract as ts
 from PIL import Image
 
 from object_detection.utils import label_map_util
@@ -14,7 +14,7 @@ import tensorflow as tf
 from config.database import db
 
 
-def class_object_detection(filename, class_comp_id, model_path, label_path, image_path):
+def class_object_detection(model_path, label_path, image_path):
     detect_fn = tf.saved_model.load(model_path)
     category_index = label_map_util.create_category_index_from_labelmap(label_path, use_display_name=True)
     image_np = np.array(Image.open(image_path))
@@ -40,35 +40,32 @@ def class_object_detection(filename, class_comp_id, model_path, label_path, imag
 
 
 def component_separation(filename, class_comp_id):
-    boxes, num_entities, accurate_indexes, num_entities, category_index, class_id = class_object_detection(filename,
-                                                                                                           class_comp_id,
-                                                                                                           app.CLASS_SAVED_MODEL_PATH,
-                                                                                                           app.CLASS_SAVED_LABEL_PATH,
-                                                                                                           app.SUBMISSION_PATH_CLASS + '/' + filename)
+    mdl1_path = app.CLASS_SAVED_MODEL_PATH
+    lbl1_path = app.CLASS_SAVED_LABEL_PATH
+    img1_path = app.SUBMISSION_PATH_CLASS + '/' + filename
+    boxes, num_entities, accurate_indexes, num_entities, category_index, class_id = class_object_detection(mdl1_path,
+                                                                                                           lbl1_path,
+                                                                                                           img1_path)
+
     # Convert the class id in their name
     if num_entities > 0:
-        for i in range(0, len(accurate_indexes)):
+        for index in range(0, len(accurate_indexes)):
 
-            if category_index[class_id]['name'] == 'class':
-                class_image = crop_and_image_resolution(filename, boxes)
-                cv2.imwrite(app.SUBMISSION_PATH_CLASS, "class" + str(i))
-                # boxes,num_entities,accurate_indexes,num_entities,category_index,class_id = class_object_detection(class_image, class_comp_id, app.CLASS_SAVED_MODEL_PATH, app.CLASS_SAVED_LABEL_PATH,app.SUBMISSION_PATH_CLASS+'/'+"class"+str(i))
-
-            elif category_index[class_id]['name'] == 'interface':
-                image = crop_and_image_resolution(filename, boxes)
+            if category_index[class_id[index]]['name'] == 'class' or category_index[class_id]['name'] == 'interface':
+                class_details_detection(filename, boxes, index)
 
 
-def crop_and_image_resolution(filename, boxes):
-    image = cv2.imread(app.SUBMISSION_PATH + '/' + filename)
+def crop_and_image_resolution(path, boxes, index):
+    image = cv2.imread(path)
     height, width, c = image.shape
     # crop box format: xmin, ymin, xmax, ymax
-    ymin = boxes[0] * height
-    xmin = boxes[1] * width
-    ymax = boxes[2] * height
-    xmax = boxes[3] * width
+    ymin = boxes[index][0] * height
+    xmin = boxes[index][1] * width
+    ymax = boxes[index][2] * height
+    xmax = boxes[index][3] * width
 
     cropped_image = image[int(ymin):int(ymax), int(xmin):int(xmax)]
-    image = cv2.imwrite('image.jpg', cropped_image)
+    image = cv2.imwrite('image.jpg' + index, cropped_image)
     # convert values to int
     black = (0, 0, 0)
     white = (255, 255, 255)
@@ -87,8 +84,33 @@ def crop_and_image_resolution(filename, boxes):
         else:
             new_pixels.append(white)
 
-            # Create and save new image.
+    # Create and save new image.
     new_img = Image.new("RGB", img.size)
     new_img.putdata(new_pixels)
 
     return image
+
+
+def text_extraction(image):
+    config = ('-l eng --oem 1 --psm 4')
+    text = ts.image_to_string(image, config=config)
+    return text
+
+
+def class_details_detection(filename, boxes, index):
+    _image = crop_and_image_resolution(filename, boxes, index)
+    cv2.imwrite(app.SUBMISSION_PATH_CLASS + '/' + "class" + str(index), _image)
+    mdl2_path = app.CLASS_COMP_SAVED_MODEL_PATH
+    lbl2_path = app.CLASS_COMP_SAVED_LABEL_PATH
+    img2_path = app.SUBMISSION_PATH_CLASS + '/' + "class" + str(index)
+    boxes_class, num_entities, accurate_indexes, num_entities, category_index, class_id = class_object_detection(
+        mdl2_path, lbl2_path, img2_path)
+
+    for j in range(0, len(accurate_indexes)):
+        if category_index[class_id[j]]['name'] == 'class_attributes':
+            class_attributes = crop_and_image_resolution(img2_path, boxes_class, j)
+            text_extraction(class_attributes)
+        elif category_index[class_id[j]]['name'] == 'class_methods':
+            class_methods = crop_and_image_resolution(img2_path, boxes_class, j)
+            text_extraction(class_methods)
+        
