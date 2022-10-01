847c7ac84d6ed3fcb9a58145de7d5f9ea58ddcc1
Weerasinghe D.N.H <it19070704@my.sliit.lk>
2022-10-01 15:44:07 +0530

    BACKEND : new model added to tensorflow_models

--- backend/services/class_model_detection_service.py
+++ backend/services/class_model_detection_service.py
@@ -28,22 +28,29 @@ def component_separation(filename, class_comp_id):
     image_nparray = np.array(Image.open(img1_path))
 
     # print(img1_path)
-    boxes, num_entities, accurate_indexes, num_entities, category_index, class_id = class_object_detection(mdl1_path,
-                                                                                                           lbl1_path,
-                                                                                                           image_nparray)
+    boxes, accurate_indexes, category_index, class_id = class_object_detection(mdl1_path,
+                                                                               lbl1_path,
+                                                                               image_nparray)
 
     # Convert the class id in their name
-    if num_entities > 0:
-        for index in range(0, len(accurate_indexes)):
-            if category_index[class_id[index]]['name'] == 'class':
-                print(filename)
-                _image = crop_image_(image_nparray, boxes, index)
-                _image = cv2.resize(_image, None, fx=2, fy=2)
-                class_details_detection(_image, class_comp_id)
+    for index in range(0, len(accurate_indexes)):
+        if len(accurate_indexes) > 1:
+            category = category_index[class_id[index]]['name']
 
-            elif category_index[class_id[index]]['name'] == 'interface':
-                _image = crop_image_(image_nparray, boxes, index)
-                _image = cv2.resize(_image, None, fx=2, fy=2)
+        elif len(accurate_indexes) == 1:
+            category = category_index[class_id]['name']
+            print(category)
+
+        if category == 'class':
+            print(filename)
+            _image = crop_image_(image_nparray, boxes, index)
+            _image = cv2.resize(_image, None, fx=2, fy=2)
+            class_details_detection(_image, class_comp_id)
+
+        elif category == 'interface':
+            _image = crop_image_(image_nparray, boxes, index)
+            _image = cv2.resize(_image, None, fx=2, fy=2)
+            class_details_detection(_image, class_comp_id)
 
 
 def class_object_detection(model_path, label_path, image_nparray):
@@ -63,12 +70,11 @@ def class_object_detection(model_path, label_path, image_nparray):
 
     detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
 
-    accurate_indexes = [k for k, v in enumerate(detections['detection_scores']) if (v > 0.7)]
-    num_entities = len(accurate_indexes)
+    accurate_indexes = [k for k, v in enumerate(detections['detection_scores']) if (v > 0.8)]
 
     class_id = operator.itemgetter(*accurate_indexes)(detections['detection_classes'])
     boxes = detections['detection_boxes']
-    return boxes, num_entities, accurate_indexes, num_entities, category_index, class_id
+    return boxes, accurate_indexes, category_index, class_id
 
 
 def class_details_detection(_image, class_comp_id):
@@ -76,26 +82,34 @@ def class_details_detection(_image, class_comp_id):
 
     mdl2_path = app.CLASS_COMP_SAVED_MODEL_PATH
     lbl2_path = app.CLASS_COMP_SAVED_LABEL_PATH
-    boxes_class, num_entities, accurate_indexes, num_entities, category_index, class_id = class_object_detection(
+    boxes_class, accurate_indexes, category_index, class_id = class_object_detection(
         mdl2_path, lbl2_path, _image)
 
-    comp = class_name_detection(class_comp_id, _image, boxes_class, accurate_indexes)
+    for j in range(0, len(accurate_indexes)):
+        if len(accurate_indexes) > 1:
+            category = category_index[class_id[j]]['name']
 
-    if num_entities > 1:
-        for j in range(0, len(accurate_indexes)):
-            if category_index[class_id[j]]['name'] == 'class_attributes':
-                class_attributes = crop_image_(_image, boxes_class, j)
-                text = text_extraction(class_attributes)
-                attributes = save_attributes_methods(text, 'attribute')
-                alter_attributes_methods(attributes, comp.id)
+        # elif len(accurate_indexes) == 1:
+        #     category = category_index[class_id]['name']
+        #     print(category)
 
-            elif category_index[class_id[j]]['name'] == 'class_methods':
-                class_methods = crop_image_(_image, boxes_class, j)
-                text = text_extraction(class_methods)
-                print(text)
-                methods = save_attributes_methods(text, 'method')
-                alter_attributes_methods(methods, comp.id)
-                print(text)
+        if category == 'class_attributes':
+            print(category, 'line 96 - inside attributes')
+            class_attributes = crop_image_(_image, boxes_class, j)
+            text = text_extraction(class_attributes)
+            save_attributes_methods(text, 'attribute')
+            # alter_attributes_methods(attributes, comp.id)
+
+        elif category == 'class_methods':
+            print(category, 'line 103 - inside methods')
+            class_methods = crop_image_(_image, boxes_class, j)
+            text = text_extraction(class_methods)
+            print(text,'108 line')
+            save_attributes_methods(text, 'method')
+            # alter_attributes_methods(methods, comp.id)
+            print(text,'111 line')
+
+    comp = class_name_detection(class_comp_id, _image, boxes_class, category_index, accurate_indexes, class_id)
 
 
 def crop_image_(image, boxes, index):
@@ -124,10 +138,10 @@ def text_extraction(image):
 
 
 def save_attributes_methods(text, typ):
-    global saved_data
+    saved_data = []
     nlp = spacy.load('en_core_web_sm')
     for element in text:
-        print(element)
+        print(element,'line 145')
         # removable = str.maketrans('', '', '()')
         nlp_ner = spacy.load('ner_models/model-best')
         nlp_output = nlp_ner(element)
@@ -157,18 +171,16 @@ def save_attributes_methods(text, typ):
                     method.return_type = token.text
 
         if typ == 'attribute':
-            print(attr)
+            print(attr,'line 175 - attr')
             db.session.add(attr)
             db.session.commit()
             saved_data.append(attr)
 
         else:
-            print(method)
+            print(method,'line 181 method')
             db.session.add(method)
             db.session.commit()
             saved_data.append(method)
-
-    return saved_data
 
 
 def alter_attributes_methods(element_list, class_id):
@@ -196,20 +208,33 @@ def covert_to_access_specifier(access):
         return ''
 
 
-def crop_and_hide(image, boxes, index):
-    height, width, c = image.shape
-    for i in range(0, len(index)):
-        ymin = boxes[i][0] * height
-        xmin = boxes[i][1] * width
-        ymax = boxes[i][2] * height
-        xmax = boxes[i][3] * width
+def crop_and_hide(image, boxes, category_index, accurate_indexes, class_id):
+    print(category_index, 'category_index')
 
-        cv2.rectangle(image, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 255, 255), -1)
+    print(class_id, 'class_id')
+
+    height, width, c = image.shape
+
+    for i in range(0, len(accurate_indexes)):
+        if len(accurate_indexes) > 1:
+            category = category_index[class_id[i]]['name']
+
+        elif len(accurate_indexes) == 1:
+            category = category_index[class_id]['name']
+            print(category,'225 line')
+
+        if category != 'interface_name':
+            ymin = boxes[i][0] * height
+            xmin = boxes[i][1] * width
+            ymax = boxes[i][2] * height
+            xmax = boxes[i][3] * width
+
+            cv2.rectangle(image, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 255, 255), -1)
     return image
 
 
-def class_name_detection(class_comp_id, image, boxes, index):
-    image = crop_and_hide(image, boxes, index)
+def class_name_detection(class_comp_id, image, boxes, category_index, accurate_indexes, class_id):
+    image = crop_and_hide(image, boxes, category_index, accurate_indexes, class_id)
 
     class_name = text_extraction(image)
     if ''.join(class_name) != '':
