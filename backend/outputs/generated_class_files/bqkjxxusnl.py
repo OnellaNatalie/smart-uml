77f73f5b4299d3bd441fc46142bbc9493f40f594
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-10-07 14:05:28 +0530

    CREATE: full screen loader added

---
+++ backend/outputs/generated_class_files/bqkjxxusnl.py
@@ -0,0 +1,87 @@
+class WarehouseOperator: 
+	
+	 def create_orders_by_uploading_a_CSV_file(self): 
+		 pass 
+
+	 def make_adjustments_to_the_order(self): 
+		 pass 
+
+	 def allocate_a_vehicle_for_a_job(self): 
+		 pass 
+
+	 def change_the_system_suggested_optimized_route_if_required(self): 
+		 pass 
+
+	 def confirm_a_job(self): 
+		 pass 
+
+	 def change_the_vehicle_allocated_for_a_job(self): 
+		 pass 
+
+	 def export_information_of_a_job_to_pdf_and_xls_format(self): 
+		 pass 
+
+	 def indicate_that_the_loading_is_completed(self): 
+		 pass 
+
+	 def generate_an_invoice_for_each_vehicle(self): 
+		 pass 
+
+	 def generate_vehicle_capacity_utilization_report(self): 
+		 pass 
+
+	 def generate_outlet_wise_transport_cost_report_for_a_given_date_range(self): 
+		 pass 
+
+	 def generate_vehicle_turnaround_time_report_for_a_warehouse(self): 
+		 pass 
+
+	 def generate_cost_per_1_kg_report(self): 
+		 pass 
+
+	 def generate_an_idle_time_limit_violation_report(self): 
+		 pass 
+
+
+
+class WarehouseIncharge: 
+	
+	 def confirm_the_delivered_items_to_a_outlet(self): 
+		 pass 
+
+	 def approve_a_job(self): 
+		 pass 
+
+	 def request_a_modification_to_the_planned_mileage_of_a_job(self): 
+		 pass 
+
+
+
+class Admin: 
+	
+	 def approve_the_planned_mileage_modification_request_submitted_by_the_WarehouseIncharge(self): 
+		 pass 
+
+	 def modify_the_minimum_fix_mileage_configuration_for_a_job(self): 
+		 pass 
+
+	 def modify_the_unloading_charge(self): 
+		 pass 
+
+	 def update_the_radius_limit_from_a_warehouse_for___drop_off___charges(self): 
+		 pass 
+
+	 def set_up_the___drop_off___rate_table_based_on_number_of_outlets_visited(self): 
+		 pass 
+
+	 def modify_the___drop_off___rate_table(self): 
+		 pass 
+
+	 def edit_the_eligibility_criteria_for_driver_incentive(self): 
+		 pass 
+
+	 def edit_the_per_km_rate_for_an_existing_vehicle_capacity_type(self): 
+		 pass 
+
+
+
